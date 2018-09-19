0fdce9b2f4720f494468b62ebbde957c605b3b12
Link <inventorLink@outlook.com>
2018-09-20 01:11:33 +0800

    first commit

---
+++ app/src/main/java/org/link/dvcwallet/core/DvcService.java
@@ -0,0 +1,47 @@
+package org.link.dvcwallet.core;
+
+import android.content.Context;
+
+/**
+ * Created by liuhuanchao on 2018/9/20.
+ */
+
+public abstract class DvcService<T> {
+
+    private Context mContext;
+
+    private DvcService(){}
+
+    public DvcService(Context context){
+        this.mContext = context;
+    }
+
+    /**
+     * 创建钱包
+     * @param password
+     * @return
+     */
+    public abstract T createWallet(String password);
+
+    /**
+     * 加载钱包
+     * @param password
+     * @param walletFilePath
+     * @return
+     */
+    public abstract T loadWallet(String password, String walletFilePath);
+
+    /**
+     * 通过助记词导入钱包
+     * @param mnemonic
+     * @return
+     */
+    public abstract T loadWalletByMnemonic(String mnemonic);
+
+    /**
+     * 导出钱包
+     * @param password
+     * @return
+     */
+    public abstract T exportWallet(String password);
+}
